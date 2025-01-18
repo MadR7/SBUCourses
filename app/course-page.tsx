06c416ba44ad0528b191d3e7debd82bbaaf8628e
MadR7 <mragni108@gmail.com>
2025-01-18 16:01:02 -0500

    made sure to trim the searchInput before passing to filter

--- app/course-page.tsx
+++ app/course-page.tsx
@@ -24,7 +24,7 @@ export function CoursePage({ initialCourses, initialDepartments, initialSBCs }: 
     const currentSearch = searchParams.get("search") || "";
 
     const [searchInput, setSearchInput] = useState(currentSearch);
-    const debouncedSearchInput = useDebounce(searchInput, 250);
+    const debouncedSearchInput = useDebounce(searchInput, 250).trim();
 
     const [selectedCourses, setSelectedCourses] = useState<Course[]>([]);
     const [selectedCourseInfo, setSelectedCourseInfo] = useState<Course | null>(null);
